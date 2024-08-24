15d934f4f0a7a0c866b78ef51fcafda8efc54c6d
Shahzebdevv <shahzebalam14feb@gmail.com>
2024-08-24 17:20:35 +0530

    Homepage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Wellness from "./pages/Wellness"
+import Wellness from "./pages/Homepage"
 import './App.css'
 import SignUp from './pages/SignUp'
 
@@ -11,11 +9,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <Wellness />
-=======
-         <SignUp />
->>>>>>> 81499c4fe04e96c5c6065752c8d2857ef2a0f690
+      <Wellness />  
     </>
   )
 }
